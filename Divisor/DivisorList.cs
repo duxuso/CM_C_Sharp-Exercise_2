809fd94eeb792a28be0c5400b16201ce3494d433
duxuso <duxuso@gmail.com>
2015-08-19 10:41:05 +1000

    Commit #2 Divisor

--- Divisor/DivisorList.cs
+++ Divisor/DivisorList.cs
@@ -8,24 +8,33 @@ using System.Text.RegularExpressions;
 namespace Divisor
 {    
     /// <summary>
-    /// find divisors of an Integer
+    /// Class 'DivisorList'
     /// </summary>
     public class DivisorList
     {
-        // constant positive integer
+        // Create constant positive integer, will be used in 'isPositiveInt' 
         public const string POSITIVE_INTEGER = @"^[0-9]*[1-9][0-9]*$"; 
 
-        public DivisorList() { }
-
-        // Get divisors
+        /// <summary>
+        /// Finding divisors for an positive integer
+        ///     1. the integer must be postive, which means not 0 or -#
+        ///     2. mod algorithm, 
+        ///     when num % i equals 0, i is a divisor of num
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
         public static List<int> Divisors(int num) 
         {
+            // create List 'divisors' to store results later 
             List<int> divisors = new List<int>();
 
+            // when num is a positive integer, then %
             if (num > 0)
             {                
+                // for-clause, i -> {1,...,num+1},  
                 for (int i = 1; i < num + 1; i++)
                 {
+                    // if satisfied, put i in list
                     if (0 == num % i)
                     {
                         divisors.Add(i);
@@ -36,16 +45,25 @@ namespace Divisor
             {
                 Console.WriteLine("Integer is negative.");                
             }
+            // return the list with divisors
             return divisors;
-        }
-
-        // Check the input positive integer by regular expression
+        }           
+         
+        /// <summary>
+        /// Check the input positive integer by regular expression
+        /// </summary>
+        /// <param name="str_num"></param>
+        /// <returns></returns>
         public static bool isPositiveInt(string str_num) 
         {
             return Regex.IsMatch(str_num, POSITIVE_INTEGER);
         }
-
-        // Get divisors (user inputs)
+               
+        /// <summary>
+        /// Get divisors from user inputs, this time Divisors gets a string 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
         public static List<int> Divisors(string s)
         {
             List<int> divisors = new List<int>();
@@ -70,8 +88,11 @@ namespace Divisor
             }
             return divisors;  
         }
-
-        // print result to console
+                 
+        /// <summary>
+        /// print result to console
+        /// </summary>
+        /// <param name="l"></param>
         public static void printList(List<int> l) 
         {                                 
             List<int> list = l;
@@ -90,15 +111,18 @@ namespace Divisor
             }
         }
         
-        public static void Main()
-        {
+        /// <summary>
+        /// Call method 'Divisors'
+        /// </summary>
+        public static void Main(){
             // prepare the sample integers
             int n1 = 60;
             int n2 = 42;
             List<int> list = new List<int>();
                      
             // call divisor method  
-            list = DivisorList.Divisors(n1);
+            list = Divisors(n1);
+            //list = DivisorList.Divisors(n1);
 
             // print sample 1
             Console.WriteLine("\nDivisors of {0} is:", n1);
@@ -112,8 +136,9 @@ namespace Divisor
             // Get user input  
             Console.Write("\nEnter a positive integer:");
             string input = Console.ReadLine();
-
-            list = DivisorList.Divisors(input);
+            
+            // Call method Divisors(string s)
+            list = Divisors(input);
             Console.WriteLine("\nDivisors of \"{0}\" is:", input);
             printList(list);
 
--- DivisorTests/DivisorTests.cs
+++ DivisorTests/DivisorTests.cs
@@ -5,9 +5,15 @@ using System.Collections.Generic;
 
 namespace DivisorTest
 {
+    /// <summary>
+    /// Test Class DivisorTests
+    /// </summary>
     [TestClass]
     public class DivisorTests
     {
+        /// <summary>
+        /// The first test, when integer is 60
+        /// </summary>
         [TestMethod]
         public void Divisors_CompareDivisorsOfSixty()
         {
@@ -25,18 +31,24 @@ namespace DivisorTest
             CollectionAssert.AreEquivalent(expected_list,actual_list);
         }
 
+        /// <summary>
+        /// The second test, when integer is negative
+        /// </summary>
         [TestMethod]
         public void Divisors_CompareNegativeInteger()
         {             
             // arrange
             int int_negative = -100;                       
             List<int> expected_list = new List<int>();            
-            List<int> actual_list = Divisor.DivisorList.Divisors(int_negative);
+            List<int> actual_list = DivisorList.Divisors(int_negative);
 
             // assert
             CollectionAssert.AreEquivalent(expected_list,actual_list);
         }
 
+        /// <summary>
+        /// The third test, when string is '60', user input method
+        /// </summary>
         [TestMethod]
         public void Divisors_InputInteger() { 
             // arrange
@@ -45,31 +57,37 @@ namespace DivisorTest
             {
                 1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30, 60
             };
-            List<int> actual_list = Divisor.DivisorList.Divisors(str_sixty);
+            List<int> actual_list = DivisorList.Divisors(str_sixty);
 
             // assert
             CollectionAssert.AreEquivalent(expected_list, actual_list);
         }
 
+        /// <summary>
+        /// The fourth test, when string is '-60'
+        /// </summary>
         [TestMethod]
         public void Divisors_InputNegativeInteger()
         {
             // arrange
             string str_sixtyn = "-60";
             List<int> expected_list = new List<int>();
-            List<int> actual_list = Divisor.DivisorList.Divisors(str_sixtyn);
+            List<int> actual_list = DivisorList.Divisors(str_sixtyn);
 
             // assert
             CollectionAssert.AreEquivalent(expected_list, actual_list);
         }
 
+        /// <summary>
+        /// The fifth test, when string is empty
+        /// </summary>
         [TestMethod]
         public void Divisors_InputEmpty()
         {
             // arrange
             string str_empty = "";
             List<int> expected_list = new List<int>();
-            List<int> actual_list = Divisor.DivisorList.Divisors(str_empty);
+            List<int> actual_list = DivisorList.Divisors(str_empty);
 
             // assert
             CollectionAssert.AreEquivalent(expected_list, actual_list);
